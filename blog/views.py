6283f324cab4d63d9158cfe017052957596f8897
mohammadAzimiJozani <mohammad.Azimi.jozani@gmail.com>
2021-10-10 00:11:30 +0330

    write view for blog-single

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"blog/blog-home.html")
+
+
+def blog_single(request):
+    return render(request,"blog/blog-single.html")
