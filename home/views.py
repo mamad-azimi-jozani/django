021381f44267f1d4f1768044db78596431d3bc4a
mohammadAzimiJozani <mohammad.Azimi.jozani@gmail.com>
2021-10-10 09:12:33 +0330

    write view for each index.html and about.html and contact.html

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index_view(request):
+    return render(request,"home/index.html")
+
+def about_view(request):
+    return render(request,"home/about.html")
+
+def contact_view(request):
+    return render(request,"home/contact.html")
