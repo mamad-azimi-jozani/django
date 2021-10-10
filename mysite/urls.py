e9101dd868a7a8a43f554423268877ded66a2331
mohammadAzimiJozani <mohammad.Azimi.jozani@gmail.com>
2021-10-10 09:10:46 +0330

    create home app and add a url for it by include

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include("blog.urls"))
+    path('blog/', include("blog.urls")),
+    path('', include("home.urls")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
