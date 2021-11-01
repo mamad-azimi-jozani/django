b37b846f7675cc3d8f8d1338dc4f9977444ce088
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-11-01 11:18:09 +0330

    register post model to admin for customizing

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_date"
+    list_display = ['title','content','published_date','created_date','status']
+    list_filter = ['status']
+    # ordering = ['created_date']
+    search_fields = ['title']
