47dae5352e923356463e152ed4889cbfc1456425
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-11-01 11:51:38 +0330

    return obj for each name with magic method

--- home/models.py
+++ home/models.py
@@ -8,4 +8,5 @@ class Contact(models.Model):
     message = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
-    # pass
+    def __str__(self):
+        return self.name
