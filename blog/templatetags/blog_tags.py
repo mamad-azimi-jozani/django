39ac824d5834dfdb143cdea1a40c5e54fdd26c47
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-11-10 22:38:31 +0330

    i wrote post_category inclusion tag for showing each post how many category has.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from ..models import Post
+from ..models import Post,Category
 register = template.Library()
 
 
@@ -8,3 +8,14 @@ register = template.Library()
 def popular_post(arg=3):
     posts = Post.objects.filter(status=1).order_by("published_date")[:arg]
     return {"posts": posts}
+
+
+@register.inclusion_tag("blog/post-category.html")
+def post_category():
+    posts = Post.objects.filter(status=1)
+    categories = Category.objects.all()
+    cat_dict = {}
+    for name in categories:
+        cat_dict[name] = posts.filter(category=name).count()
+
+    return {"categories": cat_dict}
