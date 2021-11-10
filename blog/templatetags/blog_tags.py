968819583ce1b948904e11bde70c1d4014e6f066
mamad-azimi-jozani <azimi.jozani.mohammad@gmail.com>
2021-11-10 11:57:20 +0330

    created inclusion tag for showing last three popular-post

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,10 @@
 from django import template
-
+from ..models import Post
 register = template.Library()
 
 
 
+@register.inclusion_tag("blog/popular-post.html")
+def popular_post(arg=3):
+    posts = Post.objects.filter(status=1).order_by("published_date")[:arg]
+    return {"posts": posts}
